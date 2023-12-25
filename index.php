7cb243a15dd937609d3dafba1ce2ad2ac6f82216
диханбаевал <Dikhanbaev.2018@mail.ru>
2023-12-25 17:21:59 +0600

    add html

--- index.php
+++ index.php
@@ -24,3 +24,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pdo = null; 
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="index.php"> 
+            <label for="name">Your name:</label> 
+            <input type="text" id="name" name="name" placeholder = "Enter your name"><br><br> 
+            <label for="email">Your email:</label> 
+            <input type="email" id="email" name="email" placeholder = "Enter your email"><br><br> 
+            <label for="message">Your message:</label><br> 
+            <textarea id="message" name="message" rows="4" cols="50" placeholder = "enter you message and enjoy!"></textarea><br><br> 
+            <button type="submit">Send message!</button>
+    </form>
+</body>
+</html>
